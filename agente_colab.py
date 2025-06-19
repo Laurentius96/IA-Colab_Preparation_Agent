773487858280df69e2995f80c30166b2005455a4
Lorenzo <bianchi92@hotmail.com>
2025-06-19 17:50:30 -0300

    Create agente_colab.py

---
+++ agente_colab.py
@@ -0,0 +1,9 @@
+import os
+import re
+import json
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
